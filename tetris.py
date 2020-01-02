4e068cea1bba5990cc58ecc37cc6a05b83991df6
Roman <34299035+aiserlex@users.noreply.github.com>
2020-01-03 00:14:45 +0800

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -8,185 +8,7 @@ BOARD_HEIGHT = 20
 WINDOW_WIDTH = 200
 WINDOW_HEIGHT = 400
 
-FPS = 1
-
-
-class Board:
-    def __init__(self):
-        self._array = [[0 for i in range(BOARD_WIDTH)] for j in range(BOARD_HEIGHT)]
-
-    def print(self):
-        for i in self._array:
-            print(*i)
-
-    def get_cell_value(self, x, y):
-        if y < 0:
-            return 0
-        return self._array[y][x]
-
-    def set_cell_value(self, x, y, value):
-        self._array[y][x] = value
-
-    def inc_cell_value(self, x, y):
-        self._array[y][x] += 1
-
-    def del_line(self, i):
-        self._array.pop(i)
-        self._array.insert(0, [0 for i in range(BOARD_WIDTH)])
-
-    def add_tetromino(self, tetromino):
-        deleted = 0
-        for box in tetromino:
-            self.inc_cell_value(box[0], box[1])
-        for i, line in enumerate(self._array):
-            if sum(line) == BOARD_WIDTH:
-                self.del_line(i)
-                deleted += 1
-        return deleted
-
-    @property
-    def array(self):
-        return self._array
-
-
-class Tetromino:
-    def __init__(self, x, y):
-        self._figure = random.choice("SZLJOTI")
-        self._x = x
-        self._y = y
-        if self._figure == "S":
-            self._fig_data = (
-                                ((-1, 1), (0, 1), (0, 0), (1, 0)),
-                                ((1, 1), (1, 0), (0, 0), (0, -1)),
-                                ((-1, 1), (0, 1), (0, 0), (1, 0)),
-                                ((1, 1), (1, 0), (0, 0), (0, -1)),
-                             )
-        elif self._figure == "Z":
-            self._fig_data = (
-                                ((-1, 0), (0, 0), (0, 1), (1, 1)),
-                                ((-1, 1), (-1, 0), (0, 0), (0, -1)),
-                                ((-1, 0), (0, 0), (0, 1), (1, 1)),
-                                ((-1, 1), (-1, 0), (0, 0), (0, -1)),
-                             )
-        elif self._figure == "L":
-            self._fig_data = (
-                                ((-1, 0), (0, 0), (1, 0), (-1, 1)),
-                                ((0, 1), (0, 0), (0, -1), (1, 1)),
-                                ((-1, 0), (0, 0), (1, 0), (1, -1)),
-                                ((0, 1), (0, 0), (0, -1), (-1, -1)),
-                             )
-        elif self._figure == "J":
-            self._fig_data = (
-                                ((-1, 0), (0, 0), (1, 0), (1, 1)),
-                                ((0, 1), (0, 0), (0, -1), (1, -1)),
-                                ((-1, 0), (0, 0), (1, 0), (-1, -1)),
-                                ((-1, 1), (0, 1), (0, 0), (0, -1)),
-                             )
-        elif self._figure == "O":
-            self._fig_data = (
-                                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
-                                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
-                                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
-                                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
-                             )
-        elif self._figure == "T":
-            self._fig_data = (
-                                ((-1, 0), (0, 0), (1, 0), (0, 1)),
-                                ((0, 1), (0, 0), (0, -1), (1, 0)),
-                                ((-1, 0), (0, 0), (1, 0), (0, -1)),
-                                ((0, 1), (0, 0), (0, -1), (-1, 0)),
-                             )
-        elif self._figure == "I":
-            self._fig_data = (
-                                ((-2, 0), (-1, 0), (0, 0), (1, 0)),
-                                ((0, 1), (0, 0), (0, -1), (0, -2)),
-                                ((-2, 0), (-1, 0), (0, 0), (1, 0)),
-                                ((0, 1), (0, 0), (0, -1), (0, -2)),
-                              )
-
-        self._turn = 0
-        self._init_data = self._fig_data[self._turn]
-
-    def get_current_data(self):
-        curr_coords = []
-        for box in self._init_data:
-            curr_coords.append((box[0] + self._x, box[1] + self._y))
-        return curr_coords
-
-    def move_down(self):
-        self._y += 1
-
-    def can_move_down(self, board):
-        curr_data = self.get_current_data()
-        for box in curr_data:
-            if (
-                    box[1] + 1 >= BOARD_HEIGHT or
-                    board.get_cell_value(box[0], box[1] + 1)
-                ):
-                return False
-        return True
-
-    def move_left(self):
-        self._x -= 1
-
-    def can_move_left(self, board):
-        curr_data = self.get_current_data()
-        for box in curr_data:
-            if (
-                    box[0] - 1 < 0 or
-                    board.get_cell_value(box[0] - 1, box[1])
-                ):
-                return False
-        return True
-
-    def move_right(self):
-        self._x += 1
-
-    def can_move_right(self, board):
-        curr_data = self.get_current_data()
-        for box in curr_data:
-            if (
-                    box[0] + 1 >= BOARD_WIDTH or
-                    board.get_cell_value(box[0] + 1, box[1])
-                ):
-                return False
-        return True
-
-    def turn_left(self):
-        self._turn = (self._turn - 1) % 4
-        self._init_data = self._fig_data[self._turn]
-
-    def can_turn_left(self, board):
-        turned_fig = self._fig_data[(self._turn - 1) % 4]
-        for point in turned_fig:
-            if (
-                    point[0] + self._x < 0 or
-                    point[0] + self._x >= BOARD_WIDTH or
-                    point[1] + self._y >= BOARD_HEIGHT or
-                    board.get_cell_value(point[0] + self._x, point[1] + self._y)
-               ):
-                return False
-        return True
-
-    def turn_right(self):
-        self._turn = (self._turn + 1) % 4
-        self._init_data = self._fig_data[self._turn]
-
-    def can_turn_right(self, board):
-        turned_fig = self._fig_data[(self._turn + 1) % 4]
-        for point in turned_fig:
-            if (
-                    point[0] + self._x < 0 or
-                    point[0] + self._x >= BOARD_WIDTH or
-                    point[1] + self._y >= BOARD_HEIGHT or
-                    board.get_cell_value(point[0] + self._x, point[1] + self._y)
-               ):
-                return False
-        return True
-
-    @property
-    def data(self):
-        return self.get_current_data()
+FPS = 2
 
 
 class Window:
@@ -197,16 +19,27 @@ class Window:
 
     def _init_root(self):
         self._root = tk.Tk()
-        self._root.geometry(f"{WINDOW_WIDTH+2}x{WINDOW_HEIGHT+21}+500+200")
         self._root.title("Game Tetris")
+        # self._root.resizable(False, False)
+        # self._root.geometry(f"{WINDOW_WIDTH+4}x{WINDOW_HEIGHT+23}+500+200")
 
     def _init_canvas(self):
-        self._canvas = tk.Canvas(self._root, bg="grey")
-        self._canvas.pack(fill=tk.BOTH, expand=1)
+        self._canvas = tk.Canvas(
+            self._root,
+            width=WINDOW_WIDTH,
+            height=WINDOW_HEIGHT,
+            bg="grey",
+        )
+        self._canvas.pack()
         self._canvas.focus_force()
 
     def _init_status_bar(self):
-        self._statusbar = tk.Label(self._root, text="", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self._statusbar = tk.Label(
+            self._root,
+            bd=1,
+            relief=tk.SUNKEN,
+            anchor=tk.W  # текст по левому краю
+        )
         self._statusbar.pack(side=tk.BOTTOM, fill=tk.X)
 
     def set_new_status(self, status):
@@ -221,87 +54,277 @@ class Window:
         return self._canvas
 
 
-class Vizual:
+class Board:
+    def __init__(self):
+        self._data = [[0 for i in range(BOARD_WIDTH)] for j in range(BOARD_HEIGHT)]
+
+    def _set_cell_value(self, x, y, value):
+        self._data[y][x] = value
+
+    def _inc_cell_value(self, x, y):
+        self._data[y][x] += 1
+
+    def print(self):
+        for line in self._data:
+            print(*line)
+
+    def get_cell_value(self, x, y):
+        if y < 0:
+            return 0
+        return self._data[y][x]
+
+    def add_tetromino(self, tetromino):
+        for segment in tetromino.get_current_coords():
+            self._inc_cell_value(segment[0], segment[1])
+
+    def delete_full_lines(self):
+        number_of_lines = 0
+        for i, line in enumerate(self._data):
+            if sum(line) == BOARD_WIDTH:
+                self._data.pop(i)
+                self._data.insert(0, [0 for i in range(BOARD_WIDTH)])
+                number_of_lines += 1
+        return number_of_lines
+
+    @property
+    def data(self):
+        return self._data
+
+
+class Tetromino:
+    def __init__(self, x, y, figure):
+        self._x = x
+        self._y = y
+        self._orientation = 0
+        if figure == "S":
+            self._data = (
+                ((-1, 1), (0, 1), (0, 0), (1, 0)),
+                ((1, 1), (1, 0), (0, 0), (0, -1)),
+                ((-1, 1), (0, 1), (0, 0), (1, 0)),
+                ((1, 1), (1, 0), (0, 0), (0, -1)),
+            )
+        elif figure == "Z":
+            self._data = (
+                ((-1, 0), (0, 0), (0, 1), (1, 1)),
+                ((-1, 1), (-1, 0), (0, 0), (0, -1)),
+                ((-1, 0), (0, 0), (0, 1), (1, 1)),
+                ((-1, 1), (-1, 0), (0, 0), (0, -1)),
+            )
+        elif figure == "L":
+            self._data = (
+                ((-1, 0), (0, 0), (1, 0), (-1, 1)),
+                ((0, 1), (0, 0), (0, -1), (1, 1)),
+                ((-1, 0), (0, 0), (1, 0), (1, -1)),
+                ((0, 1), (0, 0), (0, -1), (-1, -1)),
+            )
+        elif figure == "J":
+            self._data = (
+                ((-1, 0), (0, 0), (1, 0), (1, 1)),
+                ((0, 1), (0, 0), (0, -1), (1, -1)),
+                ((-1, 0), (0, 0), (1, 0), (-1, -1)),
+                ((-1, 1), (0, 1), (0, 0), (0, -1)),
+            )
+        elif figure == "O":
+            self._data = (
+                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
+                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
+                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
+                ((-1, 0), (0, 0), (-1, 1), (0, 1)),
+            )
+        elif figure == "T":
+            self._data = (
+                ((-1, 0), (0, 0), (1, 0), (0, 1)),
+                ((0, 1), (0, 0), (0, -1), (1, 0)),
+                ((-1, 0), (0, 0), (1, 0), (0, -1)),
+                ((0, 1), (0, 0), (0, -1), (-1, 0)),
+            )
+        elif figure == "I":
+            self._data = (
+                ((-2, 0), (-1, 0), (0, 0), (1, 0)),
+                ((0, 1), (0, 0), (0, -1), (0, -2)),
+                ((-2, 0), (-1, 0), (0, 0), (1, 0)),
+                ((0, 1), (0, 0), (0, -1), (0, -2)),
+            )
+
+    def _can_move_left(self, board):
+        current_coords = self.get_current_coords()
+        for segment in current_coords:
+            if (
+                segment[0] - 1 < 0 or
+                board.get_cell_value(segment[0] - 1, segment[1])
+            ):
+                return False
+        return True
+
+    def _can_move_right(self, board):
+        current_coords = self.get_current_coords()
+        for segment in current_coords:
+            if (
+                segment[0] + 1 >= BOARD_WIDTH or
+                board.get_cell_value(segment[0] + 1, segment[1])
+            ):
+                return False
+        return True
+
+    def _can_turn(self, board):
+        current_coords = self.get_current_coords((self._orientation - 1) % 4)
+        for segment in current_coords:
+            if (
+                segment[0] < 0 or
+                segment[0] >= BOARD_WIDTH or
+                segment[1] >= BOARD_HEIGHT or
+                board.get_cell_value(segment[0], segment[1])
+            ):
+                return False
+        return True
+
+    def try_move_left(self, board):
+        if self._can_move_left(board):
+            self._x -= 1
+
+    def try_move_right(self, board):
+        if self._can_move_right(board):
+            self._x += 1
+
+    def try_turn(self, board):
+        if self._can_turn(board):
+            self._orientation = (self._orientation - 1) % 4
+
+    def get_current_coords(self, orientation=None):
+        if orientation is None:
+            orientation = self._orientation
+        current_coords = []
+        for segment in self._data[orientation]:
+            x = segment[0] + self._x
+            y = segment[1] + self._y
+            current_coords.append((x, y))
+        return current_coords
+
+    def can_move_down(self, board):
+        current_coords = self.get_current_coords()
+        for segment in current_coords:
+            if (
+                segment[1] + 1 >= BOARD_HEIGHT or
+                board.get_cell_value(segment[0], segment[1] + 1)
+            ):
+                return False
+        return True
+
+    def move_down(self):
+        self._y += 1
+
+
+class Visual:
     def __init__(self, canvas):
         self._canvas = canvas
 
-    def draw_block(self, block_x, block_y):
-        block_width = WINDOW_WIDTH / BOARD_WIDTH
-        block_height = WINDOW_HEIGHT / BOARD_HEIGHT
-        self._canvas.create_rectangle(block_x * block_width,
-                                      block_y * block_height,
-                                      block_x * block_width + block_width,
-                                      block_y * block_height + block_height,
-                                      fill = "green", outline="black")
+    def _draw_segment(self, x, y):
+        width = WINDOW_WIDTH / BOARD_WIDTH
+        height = WINDOW_HEIGHT / BOARD_HEIGHT
+        self._canvas.create_rectangle(
+            x * width,
+            y * height,
+            (x + 1) * width,
+            (y + 1) * height,
+            fill="green",
+            outline="black",
+        )
 
     def draw_tetromino(self, tetromino):
-        curr_data = tetromino.get_current_data()
-        for box in curr_data:
-            self.draw_block(box[0], box[1])
+        for segment in tetromino.get_current_coords():
+            self._draw_segment(segment[0], segment[1])
 
     def draw_board(self, board):
-        for y, row in enumerate(board):
+        for y, row in enumerate(board.data):
             for x, val in enumerate(row):
                 if val == 1:
-                    self.draw_block(x, y)
+                    self._draw_segment(x, y)
+
+    def add_text(self, text):
+        self._canvas.create_text(
+            WINDOW_WIDTH // 2,
+            WINDOW_HEIGHT // 2,
+            text=text,
+            justify=tk.CENTER,
+            font="Verdana 14",
+            fill="blue",
+        )
 
 
 class Game:
     def __init__(self):
+        self._fps = FPS
+        self._score = 0
+        self._deletions = 0
+        self._next_figure = [random.choice("SZLJOTI") for i in range(1)]
+
         self._window = Window()
         self._window.canvas.bind("<Key>", self._onkeypress)
+
         self._board = Board()
-        self._vizual = Vizual(self._window.canvas)
-        self._tetromino = Tetromino(5, 0)
-        self._lines = 0
-        self._score = 0
+        self._visual = Visual(self._window.canvas)
+
+
+        self._new_tetromino()
+
         self._update()
 
     def _onkeypress(self, event):
-        if event.keysym in ["Up", "w", "W"]:
-            if self._tetromino.can_turn_left(self._board):
-                self._tetromino.turn_left()
-        elif event.keysym in ["Left", "a", "A"]:
-            if self._tetromino.can_move_left(self._board):
-                self._tetromino.move_left()
+        if event.keysym in ["Left", "a", "A"]:
+            self._tetromino.try_move_left(self._board)
+
         elif event.keysym in ["Right", "d", "D"]:
-            if self._tetromino.can_move_right(self._board):
-                self._tetromino.move_right()
+            self._tetromino.try_move_right(self._board)
+
+        elif event.keysym in ["Up", "w", "W"]:
+            self._tetromino.try_turn(self._board)
+
         elif event.keysym in ["Down", "s", "S"]:
-            if self._tetromino.can_turn_right(self._board):
-                self._tetromino.turn_right()
-        elif event.keysym == "space":
-            if self._tetromino.can_move_down(self._board):
+            self._move_tetromino_down()
+
+        elif event.keysym in ["space"]:
+            while self._tetromino.can_move_down(self._board):
                 self._tetromino.move_down()
-            else:
-                lines = self._board.add_tetromino(self._tetromino.data)
-                self._lines += lines
-                self._score += lines * 4
-                self._tetromino = Tetromino(5, 0)
 
         self._visualize()
-
 
     def _update(self):
-        if self._tetromino.can_move_down(self._board):
-            self._tetromino.move_down()
-        else:
-            lines = self._board.add_tetromino(self._tetromino.data)
-            self._lines += lines
-            self._score += lines * 4
-            self._tetromino = Tetromino(5, 0)
+        self._move_tetromino_down()
+        if (score := self._board.delete_full_lines()):
+            self._score += score
+            self._deletions += 1
+            if self._deletions == 16 * (self._fps + 1 - FPS):
+                self._fps += 1
 
         self._visualize()
 
-        self._window.root.after(round(1000 / FPS), self._update)
+        if sum(self._board.data[0]) != 0:
+            self._visual.add_text("GAME OVER")
+            return
+
+        self._window.root.after(round(1000 / self._fps), self._update)
 
     def _visualize(self):
         self._window.canvas.delete("all")
-        self._vizual.draw_board(self._board.array)
-        self._vizual.draw_tetromino(self._tetromino)
 
-        status = f"Score: {self._score}; Lines: {self._lines}; FPS: {FPS}"
+        self._visual.draw_tetromino(self._tetromino)
+        self._visual.draw_board(self._board)
+
+        status = f"Score: {self._score}; "
+        status += f"FPS: {self._fps}; "
+        status += f"Next: {' '.join(self._next_figure)}"
         self._window.set_new_status(status)
+
+    def _move_tetromino_down(self):
+        if self._tetromino.can_move_down(self._board):
+            self._tetromino.move_down()
+        else:
+            self._board.add_tetromino(self._tetromino)
+            self._new_tetromino()
+
+    def _new_tetromino(self):
+        self._tetromino = Tetromino(BOARD_WIDTH // 2, 0, self._next_figure.pop(0))
+        self._next_figure.append(random.choice("SZLJOTI"))
 
     def mainloop(self):
         self._window.root.mainloop()
