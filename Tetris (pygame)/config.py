987b67254d529e2a5fd95fe5563a37c0d28ba379
Roman <34299035+aiserlex@users.noreply.github.com>
2020-07-04 17:42:25 +0800

    Add files via upload

---
+++ Tetris (pygame)/config.py
@@ -0,0 +1,17 @@
+FPS = 120
+
+# размеры окна (в пикселях)
+WINDOW_WIDTH = 200
+WINDOW_HEIGHT = 400
+
+# размеры игрового поля (в блоках)
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+
+# colors
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
